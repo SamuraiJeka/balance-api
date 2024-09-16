fedba52819c7fd2b4914c3a7048a6da1e1e582e1
SamuraiJeka <offissit123321@gmail.com>
2024-09-16 09:56:16 +0300

    Test ci

--- src/db/session.py
+++ src/db/session.py
@@ -7,5 +7,5 @@ async_engine = create_async_engine(settings.async_url, echo=True)
 async_session = async_sessionmaker(async_engine)
 
 
-class Base(DeclarativeBase): 
+class Base(DeclarativeBase):
     ...
