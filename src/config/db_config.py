c8e04915654b7cf40829d9bb8f5bfbda46d62a1c
SamuraiJeka <offissit123321@gmail.com>
2024-09-23 01:34:23 +0300

    Refactoring, tests

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     def async_url(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="../.env")
 
 
 settings = Settings()
